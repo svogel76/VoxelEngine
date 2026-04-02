f6e037000322564b7604fd73b951b4a9d06ebb50
Steffen Vogel <vogel.steffen@arcor.de>
2026-04-02 19:58:46 +0200

    git commit -m "Fix mod loading in the launcher by ensuring the mods directory is created if it doesn't exist."

--- VoxelEngine.Launcher/Program.cs
+++ VoxelEngine.Launcher/Program.cs
@@ -1,4 +1,15 @@
 using VoxelEngine.Core;
 
-var mods = new ModLoader().LoadAll("Mods/");
+string modsDirectory = ResolveModsDirectory();
+var mods = new ModLoader().LoadAll(modsDirectory);
 new EngineRunner().Run(mods);
+
+static string ResolveModsDirectory()
+{
+    // Launcher output is "Run/", while mods live one level above in "Mods/".
+    string baseDirectoryMods = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "Mods"));
+    if (Directory.Exists(baseDirectoryMods))
+        return baseDirectoryMods;
+
+    return Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Mods"));
+}
